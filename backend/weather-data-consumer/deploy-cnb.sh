7f29485c67ae62f305c14fbd75922f9806db9d07
Roman Bachmann <Roman.Bachmann@swisscom.com>
2020-04-14 21:11:59 +0200

    re-enabled targeting space during deployment

--- backend/weather-data-consumer/deploy-cnb.sh
+++ backend/weather-data-consumer/deploy-cnb.sh
@@ -3,5 +3,5 @@
 LATEST_TAG=$(docker images robachmann/weather-data-consumer --format "{{.Tag}}" | sort -r | head -n 1)
 docker push robachmann/weather-data-consumer:$LATEST_TAG
 sed -i "s/weather-data-consumer:.*/weather-data-consumer:$LATEST_TAG/g" manifest-cnb.yml
-#cf target -s backend
+cf target -s backend
 cf push -f manifest-cnb.yml
--- backend/weather-data-consumer/deploy.sh
+++ backend/weather-data-consumer/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf build
 ./gradlew assemble
-#cf target -s backend
+cf target -s backend
 cf push -f manifest.yml
